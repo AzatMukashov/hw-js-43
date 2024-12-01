c2cb4e573361e7a862d979432974a8d7b473e037
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 02:41:32 +0600

    добавил точку с запятой и отступ

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import messagesRouter from "./routes/messages";
+
 const app = express();
 const port = 8000;
 app.use(express.json());
 app.use('/messages', messagesRouter);
 app.listen(port, () => {
     console.log(`Server started on port - http://localhost:${port}`);
-})
+});
