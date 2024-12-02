9c41fb992241bab781b92bfba49fc78f866ba5db
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 18:00:57 +0600

    чтение содержимого файла и возвращение его как строки, преобразование содержимого файла из строки в JSON объект

--- routes/readFile.ts
+++ routes/readFile.ts
@@ -7,7 +7,8 @@ export const readMessages = async (): Promise<MessageData[]> => {
     const files = await fs.readdir(messagesDir);
     return Promise.all(
         files.slice(-5).map(async file => {
-
+            const content = await fs.readFile(path.join(messagesDir, file), 'utf-8');
+            return JSON.parse(content);
         })
-    )
+    );
 };
