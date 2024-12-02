2271e46f22e1679d0f415c55a949fc37cb7f5d53
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 17:05:08 +0600

    объявил асинхронную функцию

--- routes/writeFile.ts
+++ routes/writeFile.ts
@@ -3,3 +3,6 @@ import * as path from 'path';
 import {MessageData} from "../types";
 
 const messagesDir = path.join(__dirname, '../messages');
+export const writeMessage = async (message: string): Promise<MessageData> => {
+
+};
