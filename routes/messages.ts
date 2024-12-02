9315c7f5a13132b2f6c1f11c31dc2d076f8e6a75
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 16:57:56 +0600

    импортировал fs модуль с промисом, импортировал path, импортировал интерфейс

--- routes/messages.ts
+++ routes/messages.ts
@@ -1,5 +1,25 @@
 import express from "express";
-import {promises as fs} from 'fs';
-import path from 'path';
+import {writeMessage} from "./writeFile";
+import {readMessages} from "./readFile";
+
 const router = express.Router();
-const messagesDir = path.join(__dirname, '../messages');
+router.post('/create', async (req, res) => {
+    try {
+        const {message} = req.body;
+        const data = await writeMessage(message);
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+router.get('/messages', async (req, res) => {
+    try {
+        const messages = await readMessages();
+        res.json(messages);
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+export default router;
---
+++ routes/writeFile.ts
@@ -0,0 +1,3 @@
+import {promises as fs} from 'fs';
+import * as path from 'path';
+import {MessageData} from "../types";
