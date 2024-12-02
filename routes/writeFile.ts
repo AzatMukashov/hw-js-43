d05856edc5d2e6282efc08681f6212061ab9acf3
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 17:14:47 +0600

    создал переменную которая объявляет путь к директории и имя файла, заменил двоеточие на дефис для совместимости с fs

--- routes/writeFile.ts
+++ routes/writeFile.ts
@@ -5,5 +5,6 @@ import {MessageData} from "../types";
 const messagesDir = path.join(__dirname, '../messages');
 export const writeMessage = async (message: string): Promise<MessageData> => {
 const dateTime = new Date().toISOString();
+const fileName = path.join(messagesDir, `${dateTime.replace(/:/g, '-')}.txt`);
 
 };
