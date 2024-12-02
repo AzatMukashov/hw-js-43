d8c4391209ec91cef271b2af7e40fd578a54408a
Mukashov Azat <kubanichbekov2004@gmail.com>
2024-12-02 17:25:44 +0600

    делаю запись данных в файл с именем, данные преобразуются в строку, ловлю и обрабатываю возможные ошибки, возвращаю объект

--- routes/writeFile.ts
+++ routes/writeFile.ts
@@ -4,13 +4,14 @@ import {MessageData} from "../types";
 
 const messagesDir = path.join(__dirname, '../messages');
 export const writeMessage = async (message: string): Promise<MessageData> => {
-const dateTime = new Date().toISOString();
-const fileName = path.join(messagesDir, `${dateTime.replace(/:/g, '-')}.txt`);
-const data: MessageData = {message, datetime: dateTime};
-try {
-    await fs.mkdir(messagesDir, { recursive: true });
-
-} catch (e) {
-
-}
+    const dateTime = new Date().toISOString();
+    const fileName = path.join(messagesDir, `${dateTime.replace(/:/g, '-')}.txt`);
+    const data: MessageData = {message, datetime: dateTime};
+    try {
+        await fs.mkdir(messagesDir, {recursive: true});
+        await fs.writeFile(fileName, JSON.stringify(data));
+    } catch (e) {
+        throw e;
+    }
+    return data;
 };
